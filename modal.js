7c45d2d50b23e1c64e50caa2619d45f6e63bd87c
Linderman343 <leonelpala234@gmail.com>
2023-04-19 19:15:03 -0300

    transition and header

--- modal.js
+++ modal.js
@@ -1,15 +1,18 @@
 let btnLogin = document.querySelector('#iniciar');
 let openLogin = document.querySelector('#modal-login');
 let closeLogin = document.querySelector('#close-login');
+let boxSesion = document.querySelector('.sesion');
 
 btnLogin.addEventListener('click', e=>{
     e.preventDefault();
     openLogin.classList.add("active")
+    boxSesion.classList.add('none')
 })
 
 closeLogin.addEventListener('click', e=>{
     e.preventDefault()
     openLogin.classList.remove("active")
+    boxSesion.classList.remove('none')
 })
 
 let btnSignUp = document.querySelector('#Registrarse');
@@ -19,9 +22,11 @@ let closeSignUp = document.querySelector('#close-SignUp');
 btnSignUp.addEventListener('click', e=>{
     e.preventDefault();
     openSignUp.classList.add("active")
+    boxSesion.classList.add('none')
 })
 
 closeSignUp.addEventListener('click', e=>{
     e.preventDefault()
     openSignUp.classList.remove("active")
+    boxSesion.classList.remove('none')
 })
